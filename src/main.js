d5109a17dd3273a61391184496c263fbfb9c36ca
bjxl <betty@jaylin.local>
2014-09-22 15:15:37 -0700

    added bottom icon images

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ define(function(require, exports, module) {
     var Modifier = require('famous/core/Modifier');
     var HeaderFooterLayout = require('famous/views/HeaderFooterLayout');
     var GridLayout = require('famous/views/GridLayout');
-    var StateModifier = ('famous/modifiers/StateModifier');
+    var View = require('famous/core/View');
     var Surface = require('famous/core/Surface');
     var Transform = require('famous/core/Transform');
     var ImageSurface = require('famous/surfaces/ImageSurface');
@@ -24,7 +24,7 @@ define(function(require, exports, module) {
     function createLayout() {
         layout = new HeaderFooterLayout({
             headerSize: 100,
-            footerSize: 50
+            footerSize: 100
         });
 
         mainContext.add(layout);
@@ -45,49 +45,67 @@ define(function(require, exports, module) {
     }
 
     function addContent() {
-        layout.content.add(new Surface({
-            content: "Content",
+        layout.content.add(new ImageSurface({
+            size: [400, 400],
+            content: "http://confrazzled.com/wp-content/uploads/2014/08/cat2.jpg",
             properties: {
                 lineHeight: '400px',
                 textAlign: "center"
             }
         }));
+
+        /*layout.content.add(new ImageSurface({
+            size: [400, 400],
+            content: "http://exmoorpet.com/wp-content/uploads/2012/08/cat.png",
+            properties: {
+                lineHeight: '400px',
+                textAlign: "center"
+            }
+        }));
+        */
     }
 
     function addFooter() {
-        layout.footer.add(createGrid ('footer', [5, 1] ));
-    }
-
-    function createGrid(section, dimensions) {
         var grid = new GridLayout({
-            dimensions: dimensions
+            dimensions: [5, 1]
         });
 
-        var surfaces = [];
-        grid.sequenceFrom(surfaces);
+        var views = [];
+        grid.sequenceFrom(views);
 
-        for(var i = 0; i < dimensions[0]; i++) {
-            surfaces.push(new Surface({
-                size: [undefined, undefined],
+        var images =[];
+        images[0] = 'http://irishnewsreview.files.wordpress.com/2014/08/home.png';
+        images[1] = 'https://cdn2.iconfinder.com/data/icons/flaticons-solid/18/explore-1-512.png';
+        images[2] = 'http://cdns2.freepik.com/free-photo/vintage--squared-camera_318-10040.jpg';
+        images[3] = 'http://us.cdn3.123rf.com/168nwm/kanate/kanate1401/kanate140100048/25238361-heart-symbol-speech-bubble-illustration.jpg';
+        images[4] = 'http://icons.iconarchive.com/icons/custom-icon-design/mono-business-2/512/news-icon.png';
+
+        for(var i = 0; i < 5; i++) {
+            var view = new View();
+
+            var centerModifier = new Modifier({
+                origin: [0.5, 0.5]
+            });
+
+            var surface = new ImageSurface({
+                content: images[i],
+                size: [75, 75],
                 properties: {
-                    backgroundColor: "hsl(" + (i * 360 / 8) + ", 100%, 50%)",
-                    color: "#F2F2F2",
+                    //color: "black",
                     lineHeight: '50px',
                     textAlign: 'center'
                 }
-            }));
-        }
+            });
 
-        surfaces[0].setContent('home');
-        surfaces[1].setContent('explore');
-        surfaces[2].setContent('camera');
-        surfaces[3].setContent('news');
-        surfaces[4].setContent('user');
+            view.add(centerModifier).add(surface);
 
-        return grid;
+            views.push(view);
+        };
 
-    }
+        layout.footer.add(grid);
+    };
 
 
-    mainContext.add(layout);
+
+
 });
