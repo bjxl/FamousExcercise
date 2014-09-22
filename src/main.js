a0bf12555d51941b2768aa0cde4843196e8db224
Betty <betty@jaylin.local>
2014-09-22 13:38:42 -0700

    instagram skeleton layout

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ define(function(require, exports, module) {
     // import dependencies
     var Engine = require('famous/core/Engine');
     var Modifier = require('famous/core/Modifier');
+    var HeaderFooterLayout = require('famous/views/HeaderFooterLayout');
+    var GridLayout = require('famous/views/GridLayout');
+    var StateModifier = ('famous/modifiers/StateModifier');
+    var Surface = require('famous/core/Surface');
     var Transform = require('famous/core/Transform');
     var ImageSurface = require('famous/surfaces/ImageSurface');
 
@@ -9,19 +13,81 @@ define(function(require, exports, module) {
     var mainContext = Engine.createContext();
 
     // your app here
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: 'http://code.famo.us/assets/famous_logo.svg',
-        classes: ['double-sided']
-    });
+    var layout;
 
-    var initialTime = Date.now();
-    var centerSpinModifier = new Modifier({
-        origin: [0.5, 0.5],
-        transform : function(){
-            return Transform.rotateY(.002 * (Date.now() - initialTime));
+    createLayout();
+    addHeader();
+    addContent();
+    addFooter();
+
+
+    function createLayout() {
+        layout = new HeaderFooterLayout({
+            headerSize: 100,
+            footerSize: 50
+        });
+
+        mainContext.add(layout);
+    }
+
+
+    function addHeader() {
+        layout.header.add(new Surface({
+            content: "Instagram",
+            properties: {
+                backgroundColor: '#2E2EFE',
+                fontSize: '50px',
+                color: '#F2F2F2',
+                lineHeight: '100px',
+                textAlign: 'left'
+            }
+        }));
+    }
+
+    function addContent() {
+        layout.content.add(new Surface({
+            content: "Content",
+            properties: {
+                lineHeight: '400px',
+                textAlign: "center"
+            }
+        }));
+    }
+
+    function addFooter() {
+        layout.footer.add(createGrid ('footer', [5, 1] ));
+    }
+
+    function createGrid(section, dimensions) {
+        var grid = new GridLayout({
+            dimensions: dimensions
+        });
+
+        var surfaces = [];
+        grid.sequenceFrom(surfaces);
+
+        for(var i = 0; i < dimensions[0]; i++) {
+            surfaces.push(new Surface({
+                size: [undefined, undefined],
+                properties: {
+                    backgroundColor: "hsl(" + (i * 360 / 8) + ", 100%, 50%)",
+                    color: "#F2F2F2",
+                    lineHeight: '50px',
+                    textAlign: 'center'
+                }
+            }));
         }
-    });
 
-    mainContext.add(centerSpinModifier).add(logo);
+        surfaces[0].setContent('home');
+        surfaces[1].setContent('explore');
+        surfaces[2].setContent('camera');
+        surfaces[3].setContent('news');
+        surfaces[4].setContent('user');
+
+        return grid;
+
+    }
+
+
+    mainContext.add(layout);
 });
